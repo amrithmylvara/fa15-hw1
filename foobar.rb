9d98e4507fee886fdfa1105582af086b9ed051f0
amrithmylvara <amrithmylvara19@berkeley.edu>
2016-02-26 01:59:57 -0800

    Hw1 resubmit

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
-  # Q4 CODE HERE
+	def Foobar.baz(arr)
+		ints = arr.collect { |x| Integer(x)}
+		intstwo = ints.collect { |x| x + 2 }
+		intsthree = intstwo.delete_if { |x| x%2 != 0 }
+		intsfour = intsthree.uniq
+		intsfive = intsfour.delete_if { |x| x > 10 }
+		intsfive.inject(:+)
+	end
 end
